bf144664381a9931d232ed42d63e2cf05e35c7a4
jultimrova@gmail.com <splntrs019>
2021-07-28 21:20:30 +0300

    created quiz box and footer

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 const TIME = 15
 
-const rootNode = document.getElementById('root')
+const app = document.getElementById('app')
 const container = document.createElement('div')
 container.classList.add('container')
 
@@ -30,23 +30,92 @@ exitButton.classList.add('quit-btn')
 exitButton.innerHTML = 'Exit Quiz'
 
 const restartButton = document.createElement('button')
-restartButton.classList.add('restart-btn')
-restartButton.innerHTML = 'Restart Quiz'
+restartButton.classList.add('continue-btn')
+restartButton.innerHTML = 'Continue'
 
 const infoText = `
-	<div>1. You will have only <span>${TIME}</span> seconds per each question.</div>
+	<div>1. You will have only <span>${TIME} seconds</span> per each question.</div>
 	<div>2. Once you select your answer, it can't be undone.</div>
 	<div>3. You can't select any option once time goes off</div>
 	<div>4. You can't exit from the Quiz while you're playing.</div>
 	<div>5. You'll get points on the basis of your correct answers.</div>
 `
 
-rootNode.append(container)
+// * Create Quiz container
+
+const quizBox = document.createElement('div')
+quizBox.classList.add('quiz-box')
+
+const header = document.createElement('header')
+const quizTitle = document.createElement('h1')
+quizTitle.classList.add('quiz-box__title')
+quizTitle.innerHTML = 'Quiz Application'
+
+const quizTimer = document.createElement('div')
+quizTimer.classList.add('quiz-box__timer')
+
+const timerText = document.createElement('div')
+timerText.classList.add('timer__text')
+timerText.innerHTML = 'Time left'
+
+const time = document.createElement('div')
+time.classList.add('timer__time')
+time.innerHTML = `${TIME}`
+
+const question = `
+	<section>
+		<div class='question__text'>
+			<h3>Aang becomes an airbending master at the young age of _____</h3>
+		</div>
+		<div class='answer-list'>
+			<div class='answer'>
+				<span>12</span>
+				<div class='icon'>
+					<i class="fas fa-check"></i>
+				</div>
+			</div>
+			<div class='answer'>
+				<span>6</span>
+				<div class='icon'>
+					<i class="fas fa-times"></i>
+				</div>
+			</div>
+			<div class='answer'>
+				<span>10</span>
+			</div>
+			<div class='answer'>
+				<span>16</span>
+			</div>
+		</div>
+	</section>
+`
+
+// * Create quiz footer
+const footer = document.createElement('footer')
+const total = `
+	<div class='total-questions'>
+		<span><p>2</p><p>of</p><p>35</p><p>Questions</p></span>
+	</div>
+	<button class='next-btn'>Next</button>
+`
+app.append(container)
 container.append(startQuizButton)
-container.append(infoTitle)
+//info
 container.append(infoBox)
+infoBox.append(infoTitle)
 infoBox.append(infoList)
 infoList.insertAdjacentHTML('beforeend', infoText)
 infoList.append(buttons)
 buttons.append(exitButton)
 buttons.append(restartButton)
+//quiz
+container.append(quizBox)
+quizBox.append(header)
+header.append(quizTitle)
+header.append(quizTimer)
+quizTimer.append(timerText)
+quizTimer.append(time)
+quizBox.insertAdjacentHTML('beforeend', question)
+//footer
+container.append(footer)
+footer.insertAdjacentHTML('beforeend', total)
